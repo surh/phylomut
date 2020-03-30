053eb504e0ffb14b88b42f52f80218ab48eaa436
surh <sur00mx@gmail.com>
2020-03-29 22:51:40 -0700

    namespace

--- R/node_descendants.r
+++ R/node_descendants.r
@@ -5,16 +5,18 @@
 #'
 #' @return
 #' @export
+#'
+#' @importFrom magrittr %>%
 node_descendants <- function(tre, node){
   if(class(tre) != "phylo")
     stop("ERROR: tre must be of class phylo", call. = TRUE)
   if(length(node) != 1)
     stop("ERROR: only one node can be passed")
-  
+
   if(is.character(node)){
     node <- node_label_to_id(tre = tre, node = node)
   }
-  
+
   if(is.numeric(node)){
     descendants <- tre$edge[which(tre$edge[,1] == node), 2]
     res <- descendants
@@ -23,14 +25,14 @@ node_descendants <- function(tre, node){
         purrr::map(~node_children(tre = tre, node = .)) %>%
         unlist %>%
         unique
-      
+
       res <- union(res, descendants)
     }
-    
+
   }else{
     stop("ERROR: node must be numeric or character", call. = TRUE)
   }
-  
+
   return(res)
-  
+
 }
